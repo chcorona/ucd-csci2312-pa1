a105feba4d4e2f42f7d6e9076fce8c4278482b7a
chcorona <christopher.corona@ucdenver.edu>
2016-02-05 14:24:59 -0700

    finished with area.cpp

--- area.cpp
+++ area.cpp
@@ -1,4 +1,13 @@
 //
 // Created by CORONA'S on 2/5/2016.
 //
+#include <cmath>
+#include "Point.h"
+#include "PointTests.h"
 
+double computeArea(const Point &a, const Point &b, const Point &c)
+{
+    double area = (a.distanceTo(b) + b.distanceTo(c) + c.distanceTo(a))/2.0;
+
+    return sqrt(area * (area - a.distanceTo(b)) * (area - b.distanceTo(c)) * (area - c.distanceTo(a)));
+}
